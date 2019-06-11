18554edbfcd94c905192aa0ebce1f09f8ea2bd15
Khodakovsky, Dmitry <Dmitrii.Khodakovsky@Clarivate.com>
2019-06-12 01:39:23 +0300

    added comments for the HW1 #1

--- seleniumjava/src/test/java/hw1/SubtractionTest.java
+++ seleniumjava/src/test/java/hw1/SubtractionTest.java
@@ -9,12 +9,14 @@ public class SubtractionTest extends Base {
 
     @DataProvider
     public static Object[][] calcData() {
+//        TODO Why you decide use this numbers?
         return new Object[][] {{4, 2, 2}, {55, 5, 50}, {6, 7, -1}};
     }
 
     @Test(dataProvider = "calcData")
     public void subtractionTest(long minuend, long subtrahend, long difference) {
         long actual = calculator.sub(minuend, subtrahend);
+//        TODO Is this operation necessary?
         long expected = difference;
         assertEquals(actual, expected);
     }
--- seleniumjava/src/test/java/hw1/SumTest.java
+++ seleniumjava/src/test/java/hw1/SumTest.java
@@ -9,12 +9,14 @@ public class SumTest extends Base {
 
     @DataProvider
     public static Object[][] calcData() {
+        // TODO Why you decide use this numbers?
         return new Object[][] {{2, 2, 4}, {3, 7, 10}, {11, 15, 26}};
     }
 
     @Test(dataProvider = "calcData")
     public void sumTest(long summand1, long summand2, long addition) {
         long actual = calculator.sum(summand1, summand2);
+//        TODO Is this operation necessary?
         long expected = addition;
         assertEquals(actual, expected);
     }
