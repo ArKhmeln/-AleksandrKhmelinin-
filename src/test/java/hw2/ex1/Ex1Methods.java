d0adfb926446e69cf6866547d346bf8bad713e1d
Aleksandr Khmelinin <aakhmelinin@gmail.com>
2019-06-18 13:47:10 +0300

    hw2 smallfix2

--- src/test/java/hw2/ex1/Ex1Methods.java
+++ src/test/java/hw2/ex1/Ex1Methods.java
@@ -13,7 +13,7 @@ import static org.testng.AssertJUnit.assertEquals;
 
 public class Ex1Methods extends Hw2BaseTest {
     //6. Assert header section
-    public void assertHeadersection(List headerExpected) {
+    protected void assertHeadersection(List headerExpected) {
         List<WebElement> headerItems = driver.findElements(By.cssSelector("ul[class$='navbar-nav m-l8']>li>a[href]"));
         List<String> itemsNames = new ArrayList<>();
         for(WebElement element : headerItems) {
@@ -21,12 +21,11 @@ public class Ex1Methods extends Hw2BaseTest {
         }
         WebElement services = driver.findElement(By.cssSelector("ul[class$='navbar-nav m-l8'] a.dropdown-toggle"));
         itemsNames.add(2, services.getText());
-
         assertEquals(itemsNames, headerExpected);
     }
 
     //7. Assert index page images
-    public void pageImagesExistence() {
+    protected void pageImagesExistence() {
         List<WebElement> images = driver.findElements(By.
                 cssSelector("div[class='row clerafix benefits'] .benefit-icon"));
         for(WebElement element : images) {
@@ -35,7 +34,7 @@ public class Ex1Methods extends Hw2BaseTest {
     }
 
     //8. Assert icon texts
-    public void iconTextsDisplayed() {
+    protected void iconTextsDisplayed() {
         List<WebElement> iconTexts = driver.findElements(By.cssSelector(".main-content .benefit-txt"));
         List<String> actualIconText = new ArrayList<>();
         for(WebElement element : iconTexts) {
@@ -52,7 +51,7 @@ public class Ex1Methods extends Hw2BaseTest {
     }
 
     //9. Assert main header text
-    public void mainHeaderText() {
+    protected void mainHeaderText() {
         List<WebElement> mainHeaderText = driver.findElements(By.cssSelector("[class$='text-center']:nth-child(-n+2)"));
         List<String> actualMainHeaderText = new ArrayList<>();
         for(WebElement element : mainHeaderText) {
@@ -67,31 +66,31 @@ public class Ex1Methods extends Hw2BaseTest {
     }
 
     //10. Assert inframe existence
-    public void inframeExistence() {
+    protected void inframeExistence() {
         WebElement inframe = driver.findElement(By.id("iframe"));
         assertTrue(inframe.isDisplayed());
     }
 
     //11. Switch to the inframe, check EPAM logo
-    public void iframeEpamLogo() {
+    protected void iframeEpamLogo() {
         driver.switchTo().frame("iframe");
         WebElement iframeEPAMLogo = driver.findElement(By.id("epam_logo"));
         assertTrue(iframeEPAMLogo.isDisplayed());
     }
 
     //12. Switch to original window
-    public void switchToOriginalWindow() {
+    protected void switchToOriginalWindow() {
         driver.switchTo().defaultContent();
     }
 
     //13. Assert sub header text
-    public void subheaderText(String expectedSubHeader) {
+    protected void subheaderText(String expectedSubHeader) {
         WebElement subHeader = driver.findElement(By.cssSelector(".text-center>a"));
         assertEquals(subHeader.getText(), expectedSubHeader);
     }
 
     //14 Assert sub header link
-    public void subheaderLink(String expectedSubHeaderLink) {
+    protected void subheaderLink(String expectedSubHeaderLink) {
         assertTrue(driver.findElement(By.linkText("JDI GITHUB")).isDisplayed());
         WebElement subHeader = driver.findElement(By.cssSelector(".text-center>a"));
         String actualSubHeaderLink = subHeader.getAttribute("href");
@@ -99,12 +98,12 @@ public class Ex1Methods extends Hw2BaseTest {
     }
 
     //15. Assert left section
-    public void leftSectionExistence() {
+    protected void leftSectionExistence() {
         assertTrue(driver.findElement(By.id("mCSB_1")).isDisplayed());
     }
 
     //16. Assert footer
-    public void footerExistence() {
+    protected void footerExistence() {
         assertTrue(driver.findElement(By.cssSelector(".footer-bg")).isDisplayed());
     }
 }
